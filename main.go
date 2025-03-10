4ad32db68907ffa1d1e16d25182b6ff71a184748
Dan <daniel.wren38@proton.me>
2025-03-10 21:25:01 +0000

    added server start & listen

--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"log/slog"
+	"net"
+)
+
+const defaulListenAddress = ":5001"
+
+type Config struct {
+	ListenAddress string
+}
+
+type Server struct {
+	Config
+	ln net.Listener
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddress) == 0 {
+		cfg.ListenAddress = defaulListenAddress
+	}
+	return &Server{
+		Config: cfg,
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddress)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	return s.listen()
+}
+
+func (s *Server) listen() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {}
+
 func main() {
 
 }
