f6df0b06ddb1300226506922208b7f82fd7fceb8
Dan <daniel.wren38@proton.me>
2025-03-17 21:04:00 +0000

    added new test to test go-redis client with existing server

--- server_test.go
+++ server_test.go
@@ -8,6 +8,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func TestServerWithClients(t *testing.T) {
@@ -48,5 +50,40 @@ func TestServerWithClients(t *testing.T) {
 	time.Sleep(time.Second)
 	if len(server.peers) != 0 {
 		t.Errorf("expected 0 peers, got %d", len(server.peers))
+	}
+}
+
+func TestOfficialRedisClient(t *testing.T) {
+	listenAddress := ":5001"
+	server := NewServer(Config{
+		ListenAddress: listenAddress,
+	})
+	go func() {
+		log.Fatal(server.Start())
+	}()
+	time.Sleep(time.Millisecond * 400)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("localhost%s", ":5001"),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	testCases := map[string]string{
+		"key_1": "val_1",
+		"key_2": "val_2",
+		"key_3": "val_3",
+	}
+	for key, val := range testCases {
+		if err := rdb.Set(context.Background(), key, val, 0).Err(); err != nil {
+			t.Fatal(err)
+		}
+		newVal, err := rdb.Get(context.Background(), key).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if newVal != val {
+			t.Fatalf("expected %s but got %s", val, newVal)
+		}
 	}
 }
